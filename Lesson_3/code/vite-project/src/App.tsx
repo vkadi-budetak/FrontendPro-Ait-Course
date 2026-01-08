6ce4cab301f9d6e7696fded9617b9469017c6c56
Vladyslav Kravchenko <alialiqtu@gmail.com>
2026-01-08 20:12:20 +0100

    HW lection 2

--- Lesson_3/code/vite-project/src/App.tsx
+++ Lesson_3/code/vite-project/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Counter from "./components/Counter/Counter";
 import Goodbye from "./components/Goodbye/Goodbye";
 import Greetings from "./components/Greetings/Greetings";
+import Tool from "./components/Tool/Tool";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
 
 function App() {
   return (
@@ -11,9 +13,15 @@ function App() {
       <Greetings name="Vlad" />
       <Greetings name="20" />
 
+      <Tool paragraph="Important step for our journey" />
+      <ProfileCard
+        avatar="123"
+        name="VLad"
+        description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem molestiae quibusdam non sit placeat? Tempore corrupti ullam, quo ea vel ipsum non fugiat. Natus corrupti nostrum reprehenderit reiciendis fuga quaerat!"
+      />
+
       <Goodbye familyName="Блааа Блааааблаблович" score={90} />
       <Goodbye familyName="Блааа Блааааблаблович" />
-
       <Counter />
     </>
   );
